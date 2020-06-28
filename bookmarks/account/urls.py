96fab5ff7778892a9a3d3cbed406cd599db1ba91
Umed Jadhav <umed@browserstack.com>
2020-06-28 21:40:07 +0530

    Added password Change View

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -5,6 +5,8 @@ from account.views import user_login, dashboard
 urlpatterns = [
   # path('login/',user_login, name='login'),
   path('login/',auth_views.LoginView.as_view(), name='login'),
-  path('login/',auth_views.LogoutView.as_view(), name='logout'),
-  path('',dashboard, name='dashboard')
+  path('logout/',auth_views.LogoutView.as_view(), name='logout'),
+  path('',dashboard, name='dashboard'),
+  path('password_change/',auth_views.PasswordChangeView.as_view(),name='password_change'),
+  path('password_change/done/',auth_views.PasswordChangeDoneView.as_view(),name='password_change_done'),
 ]
