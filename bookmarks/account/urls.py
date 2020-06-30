270fee0de4a21e8b54dcd1be717ebe8ab87673fe
Umed Jadhav <umed@browserstack.com>
2020-06-30 22:12:09 +0530

    Implemented User Registration Flow and added link to register on login page

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from account.views import user_login, dashboard
+from account.views import user_login, dashboard, register
 
 urlpatterns = [
   # path('login/',user_login, name='login'),
@@ -13,4 +13,5 @@ urlpatterns = [
   path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
   path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
   path('reset/done/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
+  path('register/', register, name='register')
 ]
