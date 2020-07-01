e58700cb91c81c37669b2ab18b4e11d09d254e24
Umed Jadhav <umed@browserstack.com>
2020-07-01 22:40:18 +0530

    Created Image model for the app

--- bookmarks/images/models.py
+++ bookmarks/images/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Image(models.Model):
+  user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='images_created', on_delete=models.CASCADE)
+  title = models.CharField(max_length = 200)
+  slug = models.SlugField(max_length=200)
+  url = models.URLField()
+  image=models.ImageField(upload_to='images/%d/%m/%Y')
+  description = models.TextField(blank=True)
+  created_at = models.DateField(auto_now=True, db_index=True)
+  users_like = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='images_liked', blank=True)
+
+  def __str__(self):
+    return self.title
+
+  def save(self, *args, **kwargs):
+    if not  self.slug:
+      self.slug = slugify(self.title)
+    super().save(*args, **kwargs)  
