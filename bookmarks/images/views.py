11efa152ad4b57979e4b71225d18d30f0fe6e965
Umed Jadhav <umed@browserstack.com>
2020-07-04 11:07:51 +0530

    Added view to like/dislike a imag

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
+from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+
 from .models import Image
 from .forms import ImageCreateForm
 
@@ -32,3 +35,20 @@ def image_detail(request, id, slug):
     'image': image
   }
   return render(request, 'images/image/detail.html', context)
+
+@login_required
+@require_POST
+def image_like(request):
+  image_id = request.POST.get('id')
+  action = request.POST.get('action')
+  if image_id and action:
+    try :
+      image = Image.objects.get(id=image_id)
+      if action == 'like':
+        image.users_like.add(request.user)
+      else:
+        image.users_like.remove(request.users)
+      return JsonResponse({'status':'ok'})
+    except:
+      pass
+  return JsonResponse({'status':'error'})
